02c98cd5c2513db9607d71e3367205b0aa53aba5
Lee Farnell <leefarnell@mac.com>
2021-04-11 15:50:03 +0100

    Changed syntax to Async as issues with calling data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,16 @@
 const apiKey = "096b51f6d82cf2d709ac1ea8e159d2b8";
 // const oneApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly&units=metric&appid=${apiKey}`;
 
-const fetchWeatherData = (cityName) => {
-  const functionForJSON = (responseObject) => {
-    return responseObject.json();
-  };
-  const functionForApplication = (dataFromServer) => {
-    console.log(dataFromServer);
-  };
-  const functionToHandleError = (errorObject) => {
+const fetchWeatherData = async (weatherApiUrl) => {
+  try {
+    const response = await fetch(weatherApiUrl);
+
+    const serverData = await response.json();
+
+    return serverData;
+  } catch (error) {
     console.log("error");
-  };
-
-  const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
-
-  fetch(weatherApiUrl)
-    .then(functionForJSON)
-    .then(functionForApplication)
-    .catch(functionToHandleError);
+  }
 };
 
 const getLocalStorage = () => {
@@ -60,7 +53,7 @@ const buildCityList = () => {
   $("#city-list").append(ul);
 };
 
-const onSubmit = (event) => {
+const onSubmit = async (event) => {
   event.preventDefault();
 
   const cityName = $("#input-city").val();
@@ -75,33 +68,15 @@ const onSubmit = (event) => {
   buildCityList();
   $("#input-city").val("");
 
-  fetchWeatherData(cityName);
-};
+  const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
-const weatherData = {
-  cityName: "CITY NAME",
-  temperature: 0,
-  humidity: 0,
-  windSpeed: 0,
-};
+  const data = await fetchWeatherData(weatherApiUrl);
 
-const currentWeatherCard = (data) => {
-  const card = `<div class="card-body b-2">
-  <h3 class="card-title d-inline" id="city-name">${weatherData.cityName}</h3>
-  <ul class="list-unstyled">
-    <li class="pt-3">Current Temperature: ${weatherData.temperature}°C</li>
-    <li class="pt-3">Humidity: ${weatherData.humidity}</li>
-    <li class="pt-3">Wind Speed:${weatherData.windSpeed}</li>
-    <li class="pt-3">UV Index:</li>
-  </ul>
-</div>`;
-
-  $("#current-weather").append(card);
+  console.log(data);
 };
 
 const onLoad = () => {
   buildCityList();
-  currentWeatherCard();
 };
 
 $(document).ready(onLoad);
