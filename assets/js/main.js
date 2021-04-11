74f28d5eff86cbc82955c06f48e243df342cce2f
Lee Farnell <leefarnell@mac.com>
2021-04-11 12:50:38 +0100

    Local Storage linked to search bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
+const getLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  console.log(localStorageData);
+};
+
 const onSubmit = (event) => {
   event.preventDefault();
 
   const cityName = $("#input-city").val();
   console.log(cityName);
+
+  getLocalStorage();
 };
 $("#search-form").on("submit", onSubmit);
